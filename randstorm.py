7f92f51baf40a70157eda689b1d5179dfc2ecd47
RandstormBTC <153292960+RandstormBTC@users.noreply.github.com>
2023-12-08 10:23:28 -0800

    Update randstorm.py

--- randstorm.py
+++ randstorm.py
@@ -4,40 +4,35 @@ import bech32
 import base58
 import time
 import os
-from Crypto.Hash import SHA256, RIPEMD160
 import random
 import multiprocessing
 import sys
 import mmap
+from Crypto.Hash import SHA256, RIPEMD160
 from rich.console import Console
 console = Console()
 # =========================================================================================
 btc = '''
 
-    ██████╗░██╗████████╗░█████╗░░█████╗░██╗███╗░░██╗
-    ██╔══██╗██║╚══██╔══╝██╔══██╗██╔══██╗██║████╗░██║
-    ██████╦╝██║░░░██║░░░██║░░╚═╝██║░░██║██║██╔██╗██║
-    ██╔══██╗██║░░░██║░░░██║░░██╗██║░░██║██║██║╚████║
-    ██████╦╝██║░░░██║░░░╚█████╔╝╚█████╔╝██║██║░╚███║
-    ╚═════╝░╚═╝░░░╚═╝░░░░╚════╝░░╚════╝░╚═╝╚═╝░░╚══╝
-
-    ░█▀█░▀▀▄░█▀█░░░█▀▀░█▀▀░█▀█░█▀▄░█▀▀░█░█
-    ░█▀▀░▄▀░░█▀▀░░░▀▀█░█▀▀░█▀█░█▀▄░█░░░█▀█
-    ░▀░░░▀▀▀░▀░░░░░▀▀▀░▀▀▀░▀░▀░▀░▀░▀▀▀░▀░▀
+██████╗░░█████╗░███╗░░██╗██████╗░░██████╗████████╗░█████╗░██████╗░███╗░░░███╗
+██╔══██╗██╔══██╗████╗░██║██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔══██╗████╗░████║
+██████╔╝███████║██╔██╗██║██║░░██║╚█████╗░░░░██║░░░██║░░██║██████╔╝██╔████╔██║
+██╔══██╗██╔══██║██║╚████║██║░░██║░╚═══██╗░░░██║░░░██║░░██║██╔══██╗██║╚██╔╝██║
+██║░░██║██║░░██║██║░╚███║██████╔╝██████╔╝░░░██║░░░╚█████╔╝██║░░██║██║░╚═╝░██║
+╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░╚═════╝░░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝
 '''
 # ============================================================================================
 console.print(btc)
 
 class MathRandomSimulator:
-    # Equivalent to SecureRandom() used in JSBN javascript library with Math.random()
     def __init__(self, psize=32):
         self.rng_pool = bytearray()
         self.rng_pptr = 0
         self.rng_psize = psize
-        
+
         while len(self.rng_pool) < self.rng_psize:
-            t = int(random.random() * 65536) 
-            self.rng_pool.extend(t.to_bytes(2, 'big'))
+            t = int(random.random() * (2**32))
+            self.rng_pool.extend(t.to_bytes(4, 'big'))
 
         self.rng_pptr = 0
 
@@ -48,21 +43,14 @@ class MathRandomSimulator:
         result = bytes(self.rng_pool[self.rng_pptr:self.rng_pptr + size])
         self.rng_pptr += size
         return result
-        
+
 def custom_private_key_generator(rng_simulator=None):
-    # If no RNG simulator is provided, create a new MathRandomSimulator instance
     rng_simulator = MathRandomSimulator()
-
-    # Get the next 32 bytes from the RNG simulator as private key bytes
     private_key_bytes = rng_simulator.next_bytes(32)
-
-    # Convert the private key bytes to a hexadecimal string
     private_key_hex = private_key_bytes.hex()
-
-    # Return the generated private key as a hexadecimal string
     return private_key_hex
     
-def generate_P2P_address(private_key):  #Speed: 12,000 addresses per second.
+def generate_P2P_address(private_key): 
     private_key_bytes = bytes.fromhex(private_key)
     public_key = coincurve.PrivateKey(private_key_bytes).public_key.format(compressed=False)
     public_key_hash = hashlib.new('ripemd160', hashlib.sha256(public_key).digest()).hexdigest()
@@ -70,7 +58,7 @@ def generate_P2P_address(private_key):  #Speed: 12,000 addresses per second.
     checksum = hashlib.sha256(hashlib.sha256(bytes.fromhex(extended_public_key_hash)).digest()).hexdigest()[:8]
     p2pkh_address = base58.b58encode(bytes.fromhex(extended_public_key_hash + checksum))
     return p2pkh_address.decode()
-
+    
 # Shared variable to store total count
 total_keys_generated = multiprocessing.Value('i', 0)
 
@@ -81,38 +69,47 @@ def search_for_match(database, address_set, process_id, result_queue, rng_simula
     with open(file_path, 'rb') as file:
         with mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as mmapped_file:
             iteration = 0
+            keys_generated_at_start = total_keys_generated.value
+            start_time = time.time()
+            
             while True:
-                # Generate Private HEX and P2P public Key
+                # Generate Private HEX and  public Key
                 private_key = custom_private_key_generator(rng_simulator)
                 p2p_address = generate_P2P_address(private_key)
-
+                
                 # Increment the total count
                 with total_keys_generated.get_lock():
                     total_keys_generated.value += 1
 
-                # Check if it's time to print every 10,000 keys
+                # Print every 100,000 keys
                 if iteration % 100000 == 0:
-                    print(f"\rTotal Generated Keys: \033[92m{total_keys_generated.value:,.0f}\033[0m", end='', flush=True)
+                    current_time = time.time()
+                    elapsed_time = current_time - start_time
 
+                    keys_generated = total_keys_generated.value - keys_generated_at_start
+                    keys_per_second = keys_generated / elapsed_time if elapsed_time > 0 else 0
+
+                    print(f"\rGenerated Keys: \033[92m{total_keys_generated.value:,.0f}\033[0m"
+                        f" | Keys/Second: \033[92m{keys_per_second:,.0f}\033[0m", end='', flush=True)
+                        
                 if 'p2pkh_address.decode()' in p2p_address and p2p_address in mmapped_file:
                     result_queue.put({
                         'private_key_hex': private_key,
                         'address_info': p2p_address,
                     })
-                    break  # exit the loop when a match is found
+                    break 
 
                 iteration += 1  # Increment the iteration counter
 
 if __name__ == '__main__':
 
     file_path = 'P2P_addresses_December_06_2023.txt'
-    
     # Read the entire file into a list of lines
     with open(file_path, 'r') as file:
         address_list = file.readlines()
 
     num_lines = len(address_list)
-    print(f"Opening {file_path} - {num_lines:,.0f} Addresses")
+    print(f"Opening {file_path} - \033[92m{num_lines:,.0f}\033[0m Addresses")
 
     # Process the lines as needed
     address_set = set(line.strip() for line in address_list)
@@ -146,3 +143,4 @@ if __name__ == '__main__':
 
         print("\nAll processes finished.")
         sys.exit(0)
+        
